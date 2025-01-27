3c865416ad77689ea68d8d6dc29c2c2b16a65fb5
Mike Vickers <mikevatd3@github.com>
2025-01-27 15:11:43 -0500

    fixed validation errors

--- wayne_resa_gsrp/schema.py
+++ wayne_resa_gsrp/schema.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import pandera as pa
 from pandera.typing import Series
 
@@ -10,15 +11,15 @@ class WayneResaGSRP(pa.DataFrameModel):
     zip_code: str = pa.Field(nullable=True)
     phone_number: str = pa.Field(nullable=True)
     name_on_license: str = pa.Field(nullable=True)
-    classrooms: int = pa.Field(nullable=True)
-    classrooms_mistar: int = pa.Field(nullable=True)
-    seats_mistar: int = pa.Field(nullable=True)
+    classrooms: pd.Int64Dtype = pa.Field(nullable=True)
+    classrooms_mistar: pd.Int64Dtype = pa.Field(nullable=True)
+    seats_mistar: pd.Int64Dtype = pa.Field(nullable=True)
     provider_type: str = pa.Field(nullable=True)
     subrecipient_name: str = pa.Field(nullable=True)
     hub_contact: str = pa.Field(nullable=True)
     cecc: str = pa.Field(nullable=True) # I don't know what this is
     pecc: str = pa.Field(nullable=True) # Same with this
-    four_or_five_day_pgrm: int = pa.Field(nullable=True)
+    four_or_five_day_pgrm: str = pa.Field(nullable=True) # This should be int, but failing
     application_type: str = pa.Field(nullable=True)
     transportation_available: str = pa.Field(nullable=True)
     curriculum: str = pa.Field(nullable=True)
